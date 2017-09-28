5926395b1a010ceaaa343a880520404318eddbce
Fred Emmott <fe@fb.com>
2017-09-28 10:38:12 -0700

    update travis config

--- .travis.sh
+++ .travis.sh
@@ -2,8 +2,6 @@
 set -ex
 hhvm --version
 echo hhvm.jit=0 >> /etc/hhvm/php.ini
-apt-get update -y
-apt-get install -y wget curl git
 curl https://getcomposer.org/installer | hhvm --php -- /dev/stdin --install-dir=/usr/local/bin --filename=composer
 
 cd /var/source
