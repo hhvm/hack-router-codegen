7e87304b9081a4f176dbb8a77d4de7e2782343ef
Fred Emmott <fe@fb.com>
2017-10-24 08:28:25 -0700

    test with upcoming shapes changes

--- .travis.sh
+++ .travis.sh
@@ -6,8 +6,13 @@ curl https://getcomposer.org/installer | hhvm --php -- /dev/stdin --install-dir=
 
 cd /var/source
 hhvm /usr/local/bin/composer install
+
 hh_server --check $(pwd)
-hhvm vendor/bin/phpunit tests/
-if [ $(hhvm --php -r 'echo HHVM_VERSION_ID;' 2>/dev/null) -ge 32002 ]; then
-  hhvm -d hhvm.php7.all=1 vendor/bin/phpunit tests/
+hhvm -d hhvm.php7.all=0 vendor/bin/phpunit tests/
+hhvm -d hhvm.php7.all=1 vendor/bin/phpunit tests/
+
+HHVM_VERSION=$(hhvm --php -r 'echo HHVM_VERSION_ID;' 2>/dev/null);
+if [ $HHVM_VERSION -ge 32200 -a $HHVM_VERSION -lt 32300 ]; then
+  echo enable_experimental_tc_features = optional_shape_field, unknown_fields_shape_is_not_subtype_of_known_fields_shape >> .hhconfig
+  hh_server --check $(pwd)
 fi
